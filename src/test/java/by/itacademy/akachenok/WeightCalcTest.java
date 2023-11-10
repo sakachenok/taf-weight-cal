248e4858467a133c1efbaa5e1c4a3c58da2f3838
sakachenok <akachenoksveta@gmail.com>
2023-11-10 23:30:12 +0300

    the new test was added - testFormWithValidValuesHighWeightResult

--- src/test/java/by/itacademy/akachenok/WeightCalcTest.java
+++ src/test/java/by/itacademy/akachenok/WeightCalcTest.java
@@ -91,4 +91,38 @@ public class WeightCalcTest {
         String actual = smallWeightMessageWebElement.getText();
         Assertions.assertEquals("Слишком малая масса тела", actual);
     }
+
+    @Test
+    public void testFormWithValidValuesHighWeightResult() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://svyatoslav.biz/testlab/wt/index.php");
+        String nameTextBoxXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input";
+        By nameTextBoxBy = By.xpath(nameTextBoxXpath);
+        WebElement nameTextBoxWebElement = driver.findElement(nameTextBoxBy);
+        nameTextBoxWebElement.click();
+        nameTextBoxWebElement.sendKeys("Sveta");
+        String heightTextBoxXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[3]/td[2]/input";
+        By heightTextBoxXpathBy = By.xpath(heightTextBoxXpath);
+        WebElement heightTextBoxWebElement = driver.findElement(heightTextBoxXpathBy);
+        heightTextBoxWebElement.click();
+        heightTextBoxWebElement.sendKeys("161");
+        String weightTextBoxXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[4]/td[2]/input";
+        By weightTextBoxBy = By.xpath(weightTextBoxXpath);
+        WebElement weightTextBoxWebElement = driver.findElement(weightTextBoxBy);
+        weightTextBoxWebElement.click();
+        weightTextBoxWebElement.sendKeys("70");
+        String genderFemaleRadioButtonXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[5]/td[2]/input[2]";
+        By genderFemaleRadioButtonBy = By.xpath(genderFemaleRadioButtonXpath);
+        WebElement genderFemaleRadioButtonWebElement = driver.findElement(genderFemaleRadioButtonBy);
+        genderFemaleRadioButtonWebElement.click();
+        String calculateButtonXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
+        By calculateButtonBy = By.xpath(calculateButtonXpath);
+        WebElement calculateButtonWebElement = driver.findElement(calculateButtonBy);
+        calculateButtonWebElement.click();
+        String highWeightMessageXpath = "/html/body/table/tbody/tr[2]/td[2]";
+        By highWeightMessageBy = By.xpath(highWeightMessageXpath);
+        WebElement highWeightMessageWebElement = driver.findElement(highWeightMessageBy);
+        String actual = highWeightMessageWebElement.getText();
+        Assertions.assertEquals("Незначительный избыток массы тела", actual);
+    }
 }
