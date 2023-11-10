19a4b568e2bccdd82728cb9defcd31c76c422a8e
sakachenok <akachenoksveta@gmail.com>
2023-11-10 23:45:18 +0300

    the new test was added - testFormWithInvalidHeight

--- src/test/java/by/itacademy/akachenok/WeightCalcTest.java
+++ src/test/java/by/itacademy/akachenok/WeightCalcTest.java
@@ -159,4 +159,38 @@ public class WeightCalcTest {
         String actual = overweightMessageWebElement.getText();
         Assertions.assertEquals("Значительный избыток массы тела, тучность", actual);
     }
+
+    @Test
+    public void testFormWithInvalidHeight() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://svyatoslav.biz/testlab/wt/index.php");
+        String nameTextBoxXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input";
+        By nameTextBoxBy = By.xpath(nameTextBoxXpath);
+        WebElement nameTextBoxWebElement = driver.findElement(nameTextBoxBy);
+        nameTextBoxWebElement.click();
+        nameTextBoxWebElement.sendKeys("Alla");
+        String heightTextBoxXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[3]/td[2]/input";
+        By heightTextBoxXpathBy = By.xpath(heightTextBoxXpath);
+        WebElement heightTextBoxWebElement = driver.findElement(heightTextBoxXpathBy);
+        heightTextBoxWebElement.click();
+        heightTextBoxWebElement.sendKeys("350");
+        String weightTextBoxXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[4]/td[2]/input";
+        By weightTextBoxBy = By.xpath(weightTextBoxXpath);
+        WebElement weightTextBoxWebElement = driver.findElement(weightTextBoxBy);
+        weightTextBoxWebElement.click();
+        weightTextBoxWebElement.sendKeys("50");
+        String genderFemaleRadioButtonXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[5]/td[2]/input[2]";
+        By genderFemaleRadioButtonBy = By.xpath(genderFemaleRadioButtonXpath);
+        WebElement genderFemaleRadioButtonWebElement = driver.findElement(genderFemaleRadioButtonBy);
+        genderFemaleRadioButtonWebElement.click();
+        String calculateButtonXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
+        By calculateButtonBy = By.xpath(calculateButtonXpath);
+        WebElement calculateButtonWebElement = driver.findElement(calculateButtonBy);
+        calculateButtonWebElement.click();
+        String invalidHeightMessageXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[1]/td/b";
+        By invalidHeightMessageBy = By.xpath(invalidHeightMessageXpath);
+        WebElement invalidHeightMessageWebElement = driver.findElement(invalidHeightMessageBy);
+        String actual = invalidHeightMessageWebElement.getText();
+        Assertions.assertEquals("Рост должен быть в диапазоне 50-300 см.", actual);
+    }
 }
