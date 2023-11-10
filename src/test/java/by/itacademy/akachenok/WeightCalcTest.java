4e80934eda342d43388ba6914977edb2398fa8b0
sakachenok <akachenoksveta@gmail.com>
2023-11-10 13:10:24 +0300

    names of variables are corrected to start from small letters

--- src/test/java/by/itacademy/akachenok/WeightCalcTest.java
+++ src/test/java/by/itacademy/akachenok/WeightCalcTest.java
@@ -13,14 +13,14 @@ public class WeightCalcTest {
     public void testFormWithEmptyValues() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://svyatoslav.biz/testlab/wt/index.php");
-        String CalculateButtonXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
-        By CalculateButtonBy = By.xpath(CalculateButtonXpath);
-        WebElement CalculateButtonWebElement = driver.findElement(CalculateButtonBy);
-        CalculateButtonWebElement.click();
-        String FailureMessageXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[1]/td/b";
-        By FailureMessageBy = By.xpath(FailureMessageXpath);
-        WebElement FailureMessageWebElement = driver.findElement(FailureMessageBy);
-        String actual = FailureMessageWebElement.getText();
+        String calculateButtonXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
+        By calculateButtonBy = By.xpath(calculateButtonXpath);
+        WebElement calculateButtonWebElement = driver.findElement(calculateButtonBy);
+        calculateButtonWebElement.click();
+        String failureMessageXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[1]/td/b";
+        By failureMessageBy = By.xpath(failureMessageXpath);
+        WebElement failureMessageWebElement = driver.findElement(failureMessageBy);
+        String actual = failureMessageWebElement.getText();
         Assertions.assertEquals("Не указано имя.\n" + "Рост должен быть в диапазоне 50-300 см.\n" + "Вес должен быть в диапазоне 3-500 кг.\n" + "Не указан пол.", actual);
     }
 }
